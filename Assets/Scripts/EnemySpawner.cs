d5a20d8ef7b5f2b8e5e69adef81f9b826e56f1af
swampskye <swampskye@gmail.com>
2025-02-16 11:59:37 -0800

    feat: generate zombies and zombies go forward

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float spawnRate = 2f;
+    private float nextSpawnTime;
+
+    void Update()
+    {
+        if (Time.time >= nextSpawnTime)
+        {
+            SpawnEnemy();
+            nextSpawnTime = Time.time + spawnRate;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Vector2 spawnPosition = new Vector2(Random.Range(-5f, 5f), 10f); // 生成在上方
+        Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+    }
+}
